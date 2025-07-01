1c4bde66835bd50f3a05351c7ce2763ec71585fd
Lee <blee@anl.gov>
2025-07-01 14:58:03 +0900

    pcode added

--- source/eng2wl.m
+++
@@ -1,3 +0,0 @@
-function wl = eng2wl(energy)
-% x-ray energy (keV) to wavelength (A-1 unit)
-wl = 12.398./energy;
--- source/pyfind2dpeak.m
+++
@@ -1,7 +0,0 @@
-function ndt = pyfind2dpeak(dt)
-% convert matlab array into numpy array
-dtnp = py.numpy.array(dt);
-% compute image analysis
-dt = py.find2dpeaks.detect_peaks(dtnp);
-% convert python array into matlab logical array.
-ndt = logical(dt);
